546a6e67d75627a9f5f99d15d00dafd7ba448fa5
George Rolston <grolston@users.noreply.github.com>
2021-01-05 08:55:03 -0700

    init

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "------------------"
+echo "Current AWS CLI V2 Version"
+aws --version
+echo "------------------"
+
+echo "Updating AWS CLI v2..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+echo "AWS CLI v2 updated!"
+
+echo "------------------"
+echo "Current AWS CLI V2 Version"
+aws --version
+echo "------------------"
