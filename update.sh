8f09f2c97b6c6118b94c97dac2e75a3a96c02a43
George Rolston <grolston@users.noreply.github.com>
2021-01-05 18:49:15 -0700

    reducing output of zip functionality

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ echo "------------------"
 
 echo "Updating AWS CLI v2..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+unzip awscliv2.zip -q
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
 echo "AWS CLI v2 updated!"
 
